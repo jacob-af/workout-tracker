818837ad2cfbe22ac6132eb12c81d84a24885bc3
jacob-af <jfeitler@gmail.com>
2021-04-06 11:22:56 -0400

    workout model basics built out

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true
+      },
+      name: {
+        type: String,
+        trim: true
+      },
+      duration: {
+        type: Number,
+        trim: true
+      },
+      weight: {
+        type: Number,
+        trim: true
+      },
+      reps: {
+        type: Number,
+        trim: true
+      },
+      set: {
+        type: Number,
+        trim: true
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
