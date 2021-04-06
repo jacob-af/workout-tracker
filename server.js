f0d8cf4477e43be92b9df3626fe255c5696baa31
jacob-af <jfeitler@gmail.com>
2021-04-06 16:36:18 -0400

    changed port for local development to avoid clashing with other testing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/html-routes.js"));
-//app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
